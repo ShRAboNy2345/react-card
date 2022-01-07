0c8ff396915a4515ce8a02948283e7feb9e2bc78
Your Name <you@example.com>
2022-01-08 01:04:14 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from "react";
+import './App.css'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App=()=>{ 
+  //https://fakestoreapi.com/products
+  const [fake,setFake]=useState([]);
+  console.log(fake);
+  useEffect(()=>{
+    fakestore();
+    
+  },[])
+
+
+   const fakestore=async()=>{
+   const response=await fetch("https://fakestoreapi.com/products");
+  //console.log(response);
+   const jsonData=await response.json();
+   //console.log(jsonData);
+   setFake(jsonData);
+  }
+  //fakestore();
+  return(
+    <>
+    <h2>Fake API store</h2>
+    <div className="container">
+      {fake.map((values)=>{
+        return(
+          <>
+           <div className="box">
+        <div className="content">
+          <h5>{values.title
+          }</h5>
+          <p>{values.description}</p>
+        </div>
+        <img src={values.image} alt=""/>     
+         </div>
+         </>
+        )
+      })}
+    
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
